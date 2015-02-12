fb8a694e4b8e18049a3c75ddbeb735913ec8bd39
Asael Arenas <asael2@gmail.com>
2015-02-12 11:03:17 -0500

    getting paid

--- public/js/main.js
+++ public/js/main.js
@@ -27,29 +27,45 @@ window.onload = function () {
 				subMenuItem.label = elMenu[i].label;
 				subMenuItem.url = elMenu[i].url;
 
-				var itemHtml =  '<li> <a onclick="clickedMenu('+[i]+')" href="'+subMenuItem.url+'">'+subMenuItem.label+'</a> <ul class="submenu" id="submenu'+[i]+'"></ul></li>'
+				var itemHtml =  '<li> <a onclick="clickedMenu('+i+')" href="'+subMenuItem.url+'">'+subMenuItem.label+'</a> <ul class="submenu" id="submenu'+[i]+'"></ul></li>'
 				menuContainer.innerHTML += itemHtml;
 				
-				window.clickedMenu = function(e){
-					var subMenuUl = $id('submenu'+[e]);
-					console.log(this)
-					// debugger
-					if(subMenuUl.className == "submenu"){
-						subMenuUl.setAttribute("class", "submenu show");
-					}else{
-						subMenuUl.setAttribute("class", "submenu");
-					}
 
-				}
+
 				for (j = 0; j < subMenuItem.items.length; j++){
 					var submenu = $id('submenu'+[i]);
 					submenu.innerHTML += '<li><a href="'+subMenuItem.items[j].url+'">'+subMenuItem.items[j].label+'</a></li>';
 				}		
 			}else{ // NO tiene submenu
-				menuContainer.innerHTML += '<li><a href="'+elMenu[i].url+'">'+elMenu[i].label+'</a></li>';
+				menuContainer.innerHTML += '<li> <a onclick="clickedMenu('+i+')" href="'+elMenu[i].url+'">'+elMenu[i].label+'</a></li>';
 			}
 		}
 	};
+	// test of global function printed with inner-html
+	window.clickedMenu = function(e){
+		var btnsActivos = document.getElementsByClassName("activeMenu");
+		var subActivos = document.getElementsByClassName("submenu show");
+		
+		for(var a = 0; a < btnsActivos.length; a++){
+			btnsActivos[a].setAttribute("class", "");
+			subActivos[a].setAttribute("class", "submenu");
+		}
+
+		function activate(){
+			var subMenuUl = $id('submenu'+[e]);
+			var parentMenuBtn = subMenuUl.previousElementSibling;
+			parentMenuBtn.setAttribute("class", "activeMenu")
+			
+			if(subMenuUl.className == "submenu"){
+				subMenuUl.setAttribute("class", "submenu show");
+			}else{
+				subMenuUl.setAttribute("class", "submenu");
+				parentMenuBtn.setAttribute("class", "")
+			}
+		}
+
+		activate();		
+	}
 	//Request menu
 	reqMenu(); 
 
