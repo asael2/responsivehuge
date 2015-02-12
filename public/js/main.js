15a20914e7b8f3518c1f8e53db94ae480c8c7423
Asael Arenas <asael2@gmail.com>
2015-02-12 13:33:21 -0500

    refactored menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
 'use strict'
 window.onload = function () {
-	
+	var menuId;
 	function $id(id){
 		return document.getElementById(id)
-	}	
+	};
+
+	function $cln(id){
+		return document.getElementsByClassName(id)
+	};	
 	
 	function reqMenu(){	
 		var oReq = new XMLHttpRequest();
@@ -22,51 +26,56 @@ window.onload = function () {
 		var subMenuItem = {};
 		
 		for (i = 0; i < elMenu.length; i++){
+
 			if(elMenu[i].items.length !=0){ //TIENE submenu
 				subMenuItem.items = elMenu[i].items
 				subMenuItem.label = elMenu[i].label;
 				subMenuItem.url = elMenu[i].url;
 
-				var itemHtml =  '<li> <a onclick="clickedMenu('+i+')" href="'+subMenuItem.url+'">'+subMenuItem.label+'</a> <ul class="submenu" id="submenu'+[i]+'"></ul></li>'
+				var itemHtml =  '<li> <a id="menuBtn'+i+'" onclick="clickedMenu('+i+')" href="'+subMenuItem.url+'">'+subMenuItem.label+'</a> <ul class="submenu" id="submenu'+[i]+'"></ul></li>'
 				menuContainer.innerHTML += itemHtml;
 				
-
-
 				for (j = 0; j < subMenuItem.items.length; j++){
 					var submenu = $id('submenu'+[i]);
 					submenu.innerHTML += '<li><a href="'+subMenuItem.items[j].url+'">'+subMenuItem.items[j].label+'</a></li>';
 				}		
 			}else{ // NO tiene submenu
-				menuContainer.innerHTML += '<li> <a onclick="clickedMenu('+i+')" href="'+elMenu[i].url+'">'+elMenu[i].label+'</a></li>';
+				menuContainer.innerHTML += '<li> <a id="menuBtn'+i+'" onclick="clickedMenu('+i+')" href="'+elMenu[i].url+'">'+elMenu[i].label+'</a></li>';
 			}
+
 		}
 	};
-	// test of global function printed with inner-html
-	window.clickedMenu = function(e){
-		var btnsActivos = document.getElementsByClassName("activeMenu");
-		var subActivos = document.getElementsByClassName("submenu show");
-		
-		for(var a = 0; a < btnsActivos.length; a++){
-			btnsActivos[a].setAttribute("class", "");
-			subActivos[a].setAttribute("class", "submenu");
-		}
+	
+	function activateMenu(menuId){	
+		var menuBtn = $id('menuBtn'+[menuId]);
+		var subMenuUl = $id('submenu'+[menuId]);
+		//Clicked button add class 
+		menuBtn.setAttribute("class", "activeBtn")
+		// Show submenu if exist
+		subMenuUl ? subMenuUl.setAttribute("class", "submenu show") : menuBtn.setAttribute("class", "activeBtn");
+	};
 
-		function activate(){
-			var subMenuUl = $id('submenu'+[e]);
-			var parentMenuBtn = subMenuUl.previousElementSibling;
-			parentMenuBtn.setAttribute("class", "activeMenu")
-			
-			if(subMenuUl.className == "submenu"){
-				subMenuUl.setAttribute("class", "submenu show");
-			}else{
-				subMenuUl.setAttribute("class", "submenu");
-				parentMenuBtn.setAttribute("class", "")
+	function clearMenus(menuId){
+		var btnsActivos = $cln("activeBtn");
+		var subActivos = $cln("submenu show");
+		if (btnsActivos){
+			for(var a = 0; a < btnsActivos.length; a++){
+				btnsActivos[a].setAttribute("class", "");
 			}
 		}
+		if (subActivos){
+			for(var a = 0; a < subActivos.length; a++){
+				subActivos[a].setAttribute("class", "submenu");
+			}
+		}
+		activateMenu(menuId);
+	};
 
-		activate();		
-	}
+	// global function printed with inner-html, not a good practice...
+	window.clickedMenu = function(e){
+		clearMenus(e);				
+	};
+
 	//Request menu
 	reqMenu(); 
-
 }
