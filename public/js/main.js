bac0f9562f4c80213642b22b3b595501cc8738f2
Asael Arenas <asael2@gmail.com>
2015-02-12 01:52:58 -0500

    functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 'use strict'
 window.onload = function () {
 	
+	function $id(id){
+		return document.getElementById(id)
+	}	
+	
 	function reqMenu(){	
 		var oReq = new XMLHttpRequest();
 		function myResponse () {
@@ -14,25 +18,31 @@ window.onload = function () {
 
 	function renderMenu(menuObj){
 		var i, j, elMenu = menuObj.items;
-		var menuContainer = document.getElementById("menuContainer");
+		var menuContainer = $id("menuContainer");
 		var subMenuItem = {};
 		
-
-
-
 		for (i = 0; i < elMenu.length; i++){
 			if(elMenu[i].items.length !=0){ //TIENE submenu
 				subMenuItem.items = elMenu[i].items
 				subMenuItem.label = elMenu[i].label;
 				subMenuItem.url = elMenu[i].url;
+
 				var itemHtml =  '<li> <a onclick="clickedMenu('+[i]+')" href="'+subMenuItem.url+'">'+subMenuItem.label+'</a> <ul class="submenu" id="submenu'+[i]+'"></ul></li>'
 				menuContainer.innerHTML += itemHtml;
+				
 				window.clickedMenu = function(e){
-					var subMenuUl = document.getElementById('submenu'+[e]);
-					subMenuUl.className == "submenu" ? subMenuUl.setAttribute( "class", "submenu show" ) : subMenuUl.setAttribute( "class", "submenu" );
+					var subMenuUl = $id('submenu'+[e]);
+					console.log(this)
+					// debugger
+					if(subMenuUl.className == "submenu"){
+						subMenuUl.setAttribute("class", "submenu show");
+					}else{
+						subMenuUl.setAttribute("class", "submenu");
+					}
+
 				}
 				for (j = 0; j < subMenuItem.items.length; j++){
-					var submenu = document.getElementById('submenu'+[i]);
+					var submenu = $id('submenu'+[i]);
 					submenu.innerHTML += '<li><a href="'+subMenuItem.items[j].url+'">'+subMenuItem.items[j].label+'</a></li>';
 				}		
 			}else{ // NO tiene submenu
